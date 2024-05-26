7898bd380b527b3d969c16ec57acd3d43edee1f4
Aan PB <aanesiyaa@gmail.com>
2024-05-26 10:10:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const scoreIndonesia = document.getElementById("scoreIndonesia");
+const scoreThailand = document.getElementById("scoreThailand");
+const buttonScore = document.getElementById("buttonScore");
+
+function getRandomScore() {
+  return Math.floor(Math.random() * 6);
+}
+
+function randomScore() {
+  const getScoreINA = getRandomScore();
+  const getScoreTHAI = getRandomScore();
+
+  scoreIndonesia.firstElementChild.innerHTML = getScoreINA;
+  scoreThailand.firstElementChild.innerHTML = getScoreTHAI;
+  console.log("hehe");
+}
+
+buttonScore.onclick = randomScore;
